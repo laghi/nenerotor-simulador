40209be72153de03ac82497f0df3703511cd3823
Felipe Genuino <design@felipegenuino.com>
2018-06-06 02:09:38 -0300

    Update

--- js/main.js
+++ js/main.js
@@ -8,3 +8,80 @@ $(document).ready(function() {
 });
 
  
+
+
+
+
+$(document).ready(function(){ 
+
+
+    function somaModulos () {
+      
+       $(".nivel-1 path, .nivel-2 path, .nivel-3 path").css({"fill" : "#094A41"});
+       
+    }
+
+
+
+
+            $(".pai").click(function()  { 
+
+             $(".nivel-3 path.nivel3clicado").removeClass('nivel3clicado'); 
+             $(".nivel-2 path.nivel2clicado").removeClass('nivel2clicado'); 
+
+               var identificador = $(this).attr('id');
+ 
+              if($(this).hasClass('active')){
+              
+                $(this).removeClass ( 'active' ); 
+                 $(".filho-"+identificador+",  .neto-"+identificador).removeClass ( 'active' ); 
+
+              }
+              else{
+
+                somaModulos();
+                $(this).addClass ( 'active' ); 
+                $(".filho-"+identificador+",  .neto-"+identificador).addClass ( 'active' ); 
+             
+             }
+            }); 
+
+ 
+
+    // codigo para resetar as cores
+     $('#logo-eberick-v9').click(function()  { 
+            $(".active").removeClass ( 'active' ); 
+            $(".nivel-1 path").css({"fill" : "#0C3C28"});
+            $(".nivel-2 path").css({"fill" : "#197C51"});
+            $(".nivel-3 path").css({"fill" : "#26BC7B"});
+            $(".nivel-3 path.novo-recurso").css({"fill" : "#6F1000"});
+            $(".nivel-3 path.novo-recurso:hover").css({"fill" : "#7C1200"});
+            $(".nivel-3 path.nivel3clicado").removeClass('nivel3clicado'); 
+            $(".nivel-2 path.nivel2clicado").removeClass('nivel2clicado'); 
+
+        console.log ( '#logo-eberick-v9 clicado' );
+    }); 
+
+
+
+    //se clickar em qq um elemento do nivel 3
+       $('.nivel-3 path').click(function()  { 
+                    $(".nivel-3 path.nivel3clicado").removeClass('nivel3clicado'); 
+                    $(".nivel-2 path.nivel2clicado").removeClass('nivel2clicado'); 
+                    $(this).addClass('nivel3clicado'); 
+              console.log ('.nivel-3 pathclicado');
+            }); 
+
+    //se clickar em qq um elemento do nivel 2
+       $('.nivel-2 path').click(function()  { 
+                    $(".nivel-2 path.nivel2clicado").removeClass('nivel2clicado'); 
+                    $(".nivel-3 path.nivel3clicado").removeClass('nivel3clicado'); 
+                    $(this).addClass('nivel2clicado'); 
+              console.log ('.nivel-2 pathclicado');
+            }); 
+
+
+
+
+    }); //(document).ready(function()
+     
