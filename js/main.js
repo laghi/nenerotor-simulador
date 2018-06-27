7a3dbafd6ffd53b42392f6819b069a9b4758a5d5
Murilo Laghi <murilo.laghi@gmail.com>
2018-06-27 19:49:29 -0300

    Adds color changing behavior
    Implements the color changing behavior, binding the
    correspondent parts to keep the color synchronized.

--- js/main.js
+++ js/main.js
@@ -1,38 +1,116 @@
-$(document).ready(function () {
-  //adiciona classe automagica
-  $('#base path').addClass('BlackRussian');
-  $('#inferior path').addClass('color-coral');
-  $('#superior path').addClass('color-Windsor');
-  $('#lateral-inferior path').addClass('color-Manatee');
-  $('#lateral-superior path').addClass('color-Silver');
-  $('#frontal path').addClass('BlackRussian');
-});
+(function ($) {
 
+  var pathEmEdicao;
 
-$(document).ready(function () {
-  $(".toggle-aside").click(function (event) {
-    $('.simulador__aside').toggleClass('simulador__aside--open');
-    $('.simulador__aside__backdrop').toggleClass('simulador__aside__backdrop--open');
-    $('body').toggleClass('simulador__aside__backdrop__body--open');
-    event.preventDefault();
+  var bodyColors = [
+    'Kournikova',
+    'Coral',
+    'Sulu',
+    'DeepCerise',
+    'MandarianOrange',
+    'ParisM',
+    'BlackRussian',
+    'Trout',
+    'Manatee',
+    'Valhalla',
+    'RegalBlue',
+    'DarkCerulean'
+  ];
+
+  var stripeColors = [
+    'DeepCerise',
+    'Teal',
+    'Confetti',
+    'Cyprus',
+    'Apple',
+    'BurntOrange',
+    'Windsor',
+    'DarkSlate',
+    'Sirocco',
+    'RedBerry',
+    'Silver',
+    'White',
+    'DarkCerulean'
+  ];
+
+  $(function () {
+    changeColor($('#inferior path'), 'Coral');
+    changeColor($('#superior path'), 'ParisM');
+    changeColor($('#lateral-inferior path'), 'Sirocco');
+    changeColor($('#lateral-superior path'), 'Silver');
+
+    $(".toggle-aside").click(function (event) {
+      $('.simulador__aside').toggleClass('simulador__aside--open');
+      $('.simulador__aside__backdrop').toggleClass('simulador__aside__backdrop--open');
+      $('body').toggleClass('simulador__aside__backdrop__body--open');
+      event.preventDefault();
+    });
+    $('#popover-select').select2({
+      escapeMarkup: function (markup) {
+        return markup;
+      },
+      templateResult: function (data) {
+        return '<div><span style="display: inline-block; width: 15px; height: 15px; vertical-align: middle; margin-right: 10px;" class="color-' + data.text + '"></span>' + data.text + '</div>';
+      },
+      templateSelection: function (data) {
+        return data.text;
+      },
+      width: 200
+    });
+
+    $('.is__color-interactive').click(function (e) {
+      pathEmEdicao = e.target;
+      prepareSelect(pathEmEdicao);
+      var offset = $(this).offset();
+      var left = e.pageX;
+      var top = e.pageY;
+      var theHeight = $('.popover').height();
+      $('.popover').css('left', (left + 10) + 'px');
+      $('.popover').css('top', (top - (theHeight / 2) - 0) + 'px');
+      $('.popover').show();
+    });
+    $('#escolher-cor').click(function () {
+      var selectCor = $('#popover-select');
+      changeColor(pathEmEdicao, selectCor.val());
+      $('.popover').hide();
+    });
+    $('body *:not(.controle-cor)').click(function (e) {
+      var $element = $(e.target)
+      var classe = $element.attr('class') || '';
+      if ($element.is('.is__color-interactive') || $element.is('.controle-cor') || classe.toLowerCase().indexOf('select2') >= 0) {
+        return;
+      }
+      $('.popover').hide();
+    });
   });
-});
 
-$(document).ready(function () {
-  $('[data-toggle="popover"]').popover();
-});
+  function prepareSelect(e) {
+    var $element = $(e);
+    var colors = getPossibleColors($element);
+    var selectCor = $('#popover-select');
+    selectCor.empty();
+    for (var i in colors) {
+      selectCor.append('<option class="controle-cor" value="' + colors[i] + '">' + colors[i] + '</option>')
+    }
+    selectCor.val($element.data('current-color'));
+    selectCor.trigger('change');
+  }
+
+  function getPossibleColors($element) {
+    var harnessPart = $element.parent().data('harness-part');
+    if (harnessPart === 'body') {
+      return bodyColors;
+    }
+    return stripeColors;
+  }
+
+  function changeColor(e, newColor) {
+    var elementEditing = $(e).parent().find('.is__color-interactive');
+    elementEditing.removeClass('color-' + elementEditing.data('current-color'));
+    elementEditing.data('current-color', newColor);
+    elementEditing.addClass('color-' + newColor);
+  }
 
 
-$('.is__color-interactive').click(function (e) {
-  var offset = $(this).offset();
-  var left = e.pageX;
-  var top = e.pageY;
-  var theHeight = $('.popover').height();
-  $('.popover').show();
-  $('.popover').css('left', (left + 10) + 'px');
-  $('.popover').css('top', (top - (theHeight / 2) - 0) + 'px');
-});
 
-$('.popover .close').click(function (e) {
-    $('.popover').hide();
-});
+})(jQuery);
